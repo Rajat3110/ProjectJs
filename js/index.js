6160b6dc5360019d8f82306ff6ea443cdf051f86
Rajat Shukla <rajatanita31@gmail.com>
2021-12-28 14:26:00 +0530

    final changes

--- js/index.js
+++ js/index.js
@@ -1,11 +1,15 @@
-let ar=[]; 
+let ar=[];
+let ars=[]; 
+var k=1;
 function Add(){
  var n=document.getElementById("text").value;
  //console.log(n);
     ar.push(n);
+    ars.push(k);
     //console.log(ar.length);
     document.getElementById("text").value = "";
     disp();
+    k++;
 
 }
     function disp()
@@ -13,16 +17,17 @@ function Add(){
         var text="<ol style='display:flex;flex-direction: column;'>";
         for(let i=0;i<ar.length;i++)
         {
-            text +="<li>"+ar[i]+"<i class='fa fa-trash-o' onclick=del("+i+") style='margin-left:20px;'></i></li><br>";
+            text +="<li style='list-style-type:none'>"+ars[i]+"  "+ar[i]+"<i class='fa fa-trash-o' onclick=del("+i+") style='margin-left:20px;'></i></li><br>";
             //console.log(text);
         }
         text +="</ol>";
-      //  console.log(text);
+        console.log(text);
         document.getElementById("auto").innerHTML = text;
     }
     function del(i)
     {
         ar.splice(i,1);
+        ars.splice(i,1);
         document.getElementById("auto").innerHTML = "";
        // console.log(ar);
         disp();
