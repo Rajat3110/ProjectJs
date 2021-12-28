f02345ab212dbdf201c58403208014f6eb56b56c
Rajat Shukla <rajatanita31@gmail.com>
2021-12-28 14:03:52 +0530

    Bin delete function add

--- js/index.js
+++ js/index.js
@@ -1,15 +1,30 @@
 let ar=[]; 
 function Add(){
  var n=document.getElementById("text").value;
- //console.log(n);    
- ar.push(n);
- document.getElementById("text").value = "";
- var text="<ul>";
- for(let i=0;i<ar.length;i++)
- {
-     text +="<li>"+ar[i]+"</li><br>";
-     //console.log(text);
- }
- text +="</ul>";
- document.getElementById("auto").innerHTML = text;
+ //console.log(n);
+    ar.push(n);
+    //console.log(ar.length);
+    document.getElementById("text").value = "";
+    disp();
+
 }
+    function disp()
+    {
+        var text="<ol style='display:flex;flex-direction: column;'>";
+        for(let i=0;i<ar.length;i++)
+        {
+            text +="<li>"+ar[i]+"<i class='fa fa-trash-o' onclick=del("+i+") style='margin-left:20px;'></i></li><br>";
+            //console.log(text);
+        }
+        text +="</ol>";
+      //  console.log(text);
+        document.getElementById("auto").innerHTML = text;
+    }
+    function del(i)
+    {
+        ar.splice(i,1);
+        document.getElementById("auto").innerHTML = "";
+       // console.log(ar);
+        disp();
+        
+    }
